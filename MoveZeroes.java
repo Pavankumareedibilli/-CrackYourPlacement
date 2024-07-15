39597aad7c5ac4dd34907f82bf2cf9eeb3a88f1e
pavan kumar <134722737+Pavankumareedibilli@users.noreply.github.com>
2024-07-15 22:50:49 +0530

    Create MoveZeroes.java

---
+++ MoveZeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // if(nums = null || nums.length == 0 ){
+        //     return ;
+        // }
+        int pos = 0;
+        for(int num:nums){
+            if(num !=0) {
+                nums[pos] = num;
+                pos++;
+            }
+
+        }
+        while(pos<nums.length){
+            nums[pos++] = 0;
+        }
+    }
+}
